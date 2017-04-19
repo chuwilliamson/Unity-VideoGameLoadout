6915a23904b55af22cdc2a675fc781ab5328fedf
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-04-19 17:38:14 -0500

    Put my self in another corner
    Want to be able to add and remove the upgrade components to the
    Upgradable game object but not sure of a good way to do it. May just add
    add all upgrades to the object and turn them off and enable them as they
    are active

---
+++ Assets/Scripts/Game Upgrades/GameUpgradeOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUpgradeOne : MonoBehaviour, IGameUpgrade
+{
+    public void VicotryCondition()
+    {
+        if (GetComponent<UpgradableGame>().Player.Position == GetComponent<UpgradableGame>().Environment.Exit.Position)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
---
+++ Assets/Scripts/Game Upgrades/UpgradableGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UpgradableGame : MonoBehaviour
+{
+    public PlayerBehaviour Player;
+    public Grid Environment;
+
+    List<string> UpgradeNames;
+
+    public IGameUpgrade CurrentUpgrade;
+
+    public void Upgrade(IGameUpgrade upgrade)
+    {
+        if (upgrade != CurrentUpgrade)
+            CurrentUpgrade = upgrade;
+    }
+
+    void Update()
+    {
+        if (CurrentUpgrade == null)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            CurrentUpgrade.VicotryCondition();
+        }
+    }
+}
